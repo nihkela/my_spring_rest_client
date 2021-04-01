0779ca0dc5554abeb189d21ff7bcbcc61b142f50
Alexey Alekhin <nihkela@gmail.com>
2021-04-01 22:06:57 +0300

    Show employee by id in method-main

--- src/main/java/rest/App.java
+++ src/main/java/rest/App.java
@@ -11,8 +11,9 @@ public class App {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
 
         Communication communication = context.getBean("communication", Communication.class);
-        List<Employee> allEmployees = communication.getAllEmployees();
-        System.out.println(allEmployees);
+        Employee empById = communication.getEmployee(1);
+
+        System.out.println(empById);
 
 
     }
